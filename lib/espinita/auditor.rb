722ef9ef0fe924ffebe4d83f9f6af36c8595cc93
miguel michelson <miguelmichelson@gmail.com>
2013-11-04 16:36:24 -0300

    clean a bit

--- lib/espinita/auditor.rb
+++ lib/espinita/auditor.rb
@@ -2,15 +2,6 @@ module Espinita
   module Auditor 
     extend ActiveSupport::Concern
     include Espinita::AuditorBehavior
-
-    included do
-
-    end
-
-    module ClassMethods
-     
-    end
-
   end
 end
 
